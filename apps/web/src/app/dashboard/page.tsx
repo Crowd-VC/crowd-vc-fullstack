5c950f22b9a3845771731f0da0b7622b93dc3e17
Mosheur Rahman Wolied <wolied.rainbowit@gmail.com>
2025-12-04 23:00:15 +0600

    clean up

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -4,7 +4,5 @@ import { getAllPitches } from '@/db/queries/pitches';
 export default async function IndexPageClassic() {
   const pitches = await getAllPitches();
 
-  console.log(pitches);
-
   return <ClassicScreen pitches={pitches} />;
 }
